b6f2e9ff0a2ae882ae84969a2e7c3a550a72f517
limelime <limelime@users.noreply.github.com>
2019-02-21 01:49:50 +0000

    add.

---
+++ terminal/close-n-open-new-terminal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Description: Close current terminal and open a new terminal.
+# Author: Xuan Ngo
+# Require: nohup, xfce4-terminal
+ 
+# * Don't suffix command with '&'. Otherwise, command doesn't have time to complete
+#       before this current terminal closes.
+# * 2>/dev/null: Supress "nohup: ignoring input and appending output to `nohup.out'" 
+# * Adding -e COMMAND to xfce4-terminal doesn't work. Don't know why.
+nohup xfce4-terminal --geometry=190x21+0+0 2>/dev/null
+
+# Or
+# Simpler to use --hold option.
+#xfce4-terminal --geometry=190x21+0+0 --hold
+ 
+### Close current terminal.
+kill -9 $PPID
